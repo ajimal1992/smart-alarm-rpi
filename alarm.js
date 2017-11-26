b24e762bbe846aa4f322fcd3a6b7f170aa467d07
Ajimal <ajimal@hotmail.com>
2017-11-27 00:43:12 +0800

    timestamp to token

--- alarm.js
+++ alarm.js
@@ -1,19 +1,8 @@
 //Config
 var config = require('./config/config');
 
-//DETECTION persistance
-var last_ts = 0;
-
-//crypto
-var CryptoJS = require("crypto-js");
-var esp8266_msg;
-var esp8266_iv;
-
 //request
 var request = require('request');
-
-//timestamp
-var moment = require('moment');
 
 //Setup serial comms
 const SerialPort = require('serialport');
@@ -30,39 +19,27 @@ parser.on('data', function (data) { //when data received
   var datas = data.trim().split(":");
   //console.log(data);
   if(datas.length == 2){ //check if we received the correct data
-	var curr_ts = Date.now();
-	var plain_iv =  new Buffer( datas[0] , 'base64').toString('hex');
-	var iv = CryptoJS.enc.Hex.parse( plain_iv );
-	var key= CryptoJS.enc.Hex.parse( config.KEY );
-	//decrypt
-	var bytes  = CryptoJS.AES.decrypt( datas[1], key , { iv: iv} );
-	var plaintext = bytes.toString(CryptoJS.enc.Base64);
-	var decoded_b64msg =  new Buffer(plaintext , 'base64').toString('ascii');
-	var decoded_msg = new Buffer( decoded_b64msg , 'base64').toString('ascii');
-	datas = decoded_msg.trim().split(":");
-	//console.log(datas);
-	if(datas.length == 3){
-		last_ts = curr_ts; //update timestamp
-		//notify user
-		var options = {
-			url: 'https://188.166.235.223:55555/trigger',
-			rejectUnauthorized: false,
-			requestCert: false,
-			agent: false,
-			json : {hw_id: datas[0], msg: datas[1], ts: datas[2]}
-		};
-		//console.log(options);
-		request.post(options,
-			function(error,response, body){
-				//console.log(error);
-				//console.log(body);
-			}
-		);
-	}	  
+  
+	//notify user
+	var options = {
+		url: 'https://188.166.235.223:55555/trigger',
+		rejectUnauthorized: false,
+		requestCert: false,
+		agent: false,
+		json : {enc_msg: datas[1], IV: datas[0], usr: config.KEY}
+	};
+	//console.log(options);
+	request.post(options,
+		function(error,response, body){
+			//console.log(error);
+			//console.log(body);
+		}
+	);
   }
+  
+  //this loop grabs a token from the server. Although the naming convention is used as TIMESTAMP,
+  //its actually not a timestamp. The naming convention wasnt changed for the sake of making minimum changes to the code. 
   else if(datas[0]=="TS"){
-	//console.log(formatted)
-	//console.log("Send timestamp");
 	var options = {
 		url: 'https://188.166.235.223:55555/timestamp',
 		rejectUnauthorized: false,
